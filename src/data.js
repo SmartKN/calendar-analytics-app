d2a970dddfad4a9e341240d5dce727184f255897
unknown <pskishore4@gmail.com>
2025-08-27 10:32:01 +0530

    feat: add dummy JSON data and display events in calendar

---
+++ src/data.js
@@ -0,0 +1,23 @@
+
+const data = {
+  "01-09-2025": [
+    { user: "user_1", value: 1 },
+    { user: "user_2", value: 2 },
+    { user: "user_3", value: 3 },
+    { user: "user_4", value: 4 }
+  ],
+  "02-09-2025": [
+    { user: "user_1", value: 2 },
+    { user: "user_2", value: 4 },
+    { user: "user_3", value: 1 },
+    { user: "user_4", value: 5 }
+  ],
+  "03-09-2025": [
+    { user: "user_1", value: 3 },
+    { user: "user_2", value: 1 },
+    { user: "user_3", value: 4 },
+    { user: "user_4", value: 2 }
+  ]
+};
+
+export default data;
